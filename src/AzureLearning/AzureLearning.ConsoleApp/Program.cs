6b29730f6bf571db418024f52feaa4bb1acb3b3a
Thong Ho <vanthong98@gmail.com>
2023-08-07 19:30:39 +0900

    init azure learning solution add azure service bus project

---
+++ src/AzureLearning/AzureLearning.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using TShared.Azure.ServiceBus;
+
+var messageService = new MessageService();
+var sendTask = messageService.SendMessageAsync();
+var receiveTask = messageService.ReceiveMessageAsync();
+
+await Task.WhenAll(sendTask, receiveTask);
+Console.WriteLine("DONE");
---
+++ src/AzureLearning/TShared/Azure/ServiceBus/MessageService.cs
@@ -0,0 +1,95 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace TShared.Azure.ServiceBus
+{
+    public class MessageService
+    {
+        public ServiceBusClient Init()
+        {
+            var clientOptions = new ServiceBusClientOptions()
+            {
+                TransportType = ServiceBusTransportType.AmqpWebSockets
+            };
+
+            var connectionString = "Endpoint=sb://vanthong98.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=z46QrH03wXLnW6jsgAqg2fNHrz9/WQCVs+ASbFfwVO4=";
+
+            var _client = new ServiceBusClient(connectionString, clientOptions);
+
+            return _client;
+        }
+
+        public async Task SendMessageAsync()
+        {
+            var _client = Init();
+            var numOfMessages = 1000;
+            var queueName = "test";
+            var sender = _client.CreateSender(queueName);
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            var receiver = _client.CreateReceiver(queueName);
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                await sender.DisposeAsync();
+                await _client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+
+        public async Task ReceiveMessageAsync()
+        {
+            var _client = Init();
+            ServiceBusProcessor processor;
+            var queueName = "test";
+            processor = _client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+            try
+            {
+                processor.ProcessMessageAsync += MessageHandler;
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                await processor.StartProcessingAsync();
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                await processor.DisposeAsync();
+                await _client.DisposeAsync();
+            }
+
+        }
+
+        async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
