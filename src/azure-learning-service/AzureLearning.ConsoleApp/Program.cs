fddbf7145335b9e1bea9d688ceb166147cb583ca
Thong Ho <vanthong98@gmail.com>
2023-08-10 18:50:04 +0900

    update gitignore

--- src/azure-learning-service/AzureLearning.ConsoleApp/Program.cs
+++ src/azure-learning-service/AzureLearning.ConsoleApp/Program.cs
@@ -3,12 +3,3 @@ using Azure.Identity;
 using Azure.Storage.Blobs;
 using TShared.Azure.Storage;
 
-
-await TestAzureBlobService();
-
-async Task TestAzureBlobService()
-{
-    const string storageDomain = "https://vanthong98.blob.core.windows.net";
-    var blobServiceClient = new BlobServiceClient(new Uri(storageDomain), new DefaultAzureCredential());
-    var azureBlobService = new AzureBlobService(blobServiceClient);
-}
